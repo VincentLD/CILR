ffa5cb5b2ca39b72f600e702a0da5dcd957a4b8e
Vincent <vledoledec@gmail.com>
2022-03-23 15:54:16 +0100

    changement navbar/menu

--- js/front-page.js
+++ js/front-page.js
@@ -3,12 +3,14 @@ const nav__burgerButton = document.querySelector('.nav__burger');
 const nav__menu = document.querySelector('.nav__menu');
 const nav__nav = document.querySelector('.nav');
 const nav__navLinks = document.querySelectorAll('.nav__link');
+const nav__banner = document.querySelector('.banner');
 
 
 nav__burgerButton.addEventListener('click', () => {
   nav__burgerButton.classList.toggle('active');
-  nav__menu.children[0].classList.toggle('active');
+  nav__menu.children[1].classList.toggle('active');
   nav__nav.classList.toggle('nav--active');
+  nav__banner.classList.toggle('active');
 });
 
 nav__navLinks.forEach(link => 
