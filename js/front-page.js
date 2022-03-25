a3138c2df375e9b9adb2a6b129adb7db72ae122b
Vincent <vledoledec@gmail.com>
2022-03-25 16:14:06 +0100

    fix nav

--- js/front-page.js
+++ js/front-page.js
@@ -1,11 +1,9 @@
 /* NAVIGATION */
 const nav__burgerButton = document.querySelector('.nav__burger');
-const nav__menu = document.querySelector('.nav__menu');
 const nav__nav = document.querySelector('.nav');
 const nav__navLinks = document.querySelectorAll('.nav__link');
 const nav__banner = document.querySelector('.banner');
 const nav__container = document.querySelector('.nav__container');
-
 
 nav__burgerButton.addEventListener('click', () => {
   nav__burgerButton.classList.toggle('active');
@@ -18,7 +16,7 @@ nav__burgerButton.addEventListener('click', () => {
 nav__navLinks.forEach(link => 
   link.addEventListener('click', () => {
     nav__burgerButton.classList.toggle('active');
-    nav__menu.children[1].classList.toggle('active');
+    nav__container.classList.toggle('nav__container--active');
     nav__nav.classList.toggle('nav--active');
     nav__banner.classList.toggle('active');
     document.body.classList.toggle('body--active');
