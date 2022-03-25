c5b39b2add78d7568387b008d8f54fa5c5d9bec1
Vincent <vledoledec@gmail.com>
2022-03-25 15:39:56 +0100

    new mobile menu

--- js/front-page.js
+++ js/front-page.js
@@ -4,13 +4,14 @@ const nav__menu = document.querySelector('.nav__menu');
 const nav__nav = document.querySelector('.nav');
 const nav__navLinks = document.querySelectorAll('.nav__link');
 const nav__banner = document.querySelector('.banner');
+const nav__container = document.querySelector('.nav__container');
+
 
 nav__burgerButton.addEventListener('click', () => {
   nav__burgerButton.classList.toggle('active');
-  nav__menu.children[1].classList.toggle('active');
+  nav__container.classList.toggle('nav__container--active');
   nav__nav.classList.toggle('nav--active');
   nav__banner.classList.toggle('active');
-  console.log(nav__banner.classList);
   document.body.classList.toggle('body--active');
 });
 
