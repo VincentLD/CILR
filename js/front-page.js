4a05f3fbbf33ae3242e55fdc5f7e0baf6a80a6ef
Vincent <vledoledec@gmail.com>
2022-03-25 12:08:51 +0100

    refactoring headers+buttons

--- js/front-page.js
+++ js/front-page.js
@@ -1,17 +1,16 @@
 /* NAVIGATION */
 const nav__burgerButton = document.querySelector('.nav__burger');
-
 const nav__menu = document.querySelector('.nav__menu');
 const nav__nav = document.querySelector('.nav');
 const nav__navLinks = document.querySelectorAll('.nav__link');
 const nav__banner = document.querySelector('.banner');
-
 
 nav__burgerButton.addEventListener('click', () => {
   nav__burgerButton.classList.toggle('active');
   nav__menu.children[1].classList.toggle('active');
   nav__nav.classList.toggle('nav--active');
   nav__banner.classList.toggle('active');
+  console.log(nav__banner.classList);
   document.body.classList.toggle('body--active');
 });
 
@@ -23,4 +22,4 @@ nav__navLinks.forEach(link =>
     nav__banner.classList.toggle('active');
     document.body.classList.toggle('body--active');
   })
-)
+);
